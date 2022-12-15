5cbc10fc4340d999fdf035e758a48cb92be516ba
Luiz Fernando da Rocha Pimentel <fernandozlf4@gmail.com>
2022-12-15 19:53:03 -0300

    JavaScript

---
+++ js/script.js
@@ -0,0 +1,44 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const buttom = document.querySelector('.reset')
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    } ,500);
+}
+
+const loop = setInterval(() => {
+
+    const pipePosition = pipe.offsetLeft
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    
+    if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+        
+        pipe.style.animation = "none";
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = "none";
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = "./imagens/game-over.png";
+        mario.style.width = "70px"
+        mario.style.marginLeft = "50px"
+
+        buttom.style.width = "8%"
+
+        clearInterval(loop);
+    }
+
+}, 10);
+
+function recall() {
+    window.location.reload();
+}
+
+
+
+
+document.addEventListener("keydown", jump);
